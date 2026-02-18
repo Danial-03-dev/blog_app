480aafa241d854040443f959d73c644d326764b2
Danial-03-dev <danialzafarwork003@gmail.com>
2026-02-18 22:49:04 +0500

    refactor: refactor business logic into helper function

--- lib/core/utils/pick_image.dart
+++ lib/core/utils/pick_image.dart
@@ -3,8 +3,12 @@ import 'dart:typed_data';
 import 'package:image_picker/image_picker.dart';
 
 Future<Uint8List?> pickImage() async {
+  return pickImageHelper(ImagePicker());
+}
+
+Future<Uint8List?> pickImageHelper(ImagePicker picker) async {
   try {
-    final xfile = await ImagePicker().pickImage(source: ImageSource.gallery);
+    final xfile = await picker.pickImage(source: ImageSource.gallery);
 
     if (xfile == null) return null;
 
